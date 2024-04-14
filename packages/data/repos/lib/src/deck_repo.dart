b7091942ad0275b353da7e49afa8c79f2e6396e3
ltp1405 <lamtungphan1405@gmail.com>
2024-04-14 21:22:58 +0700

    basic function for DeckRepo

---
+++ packages/data/repos/lib/src/deck_repo.dart
@@ -0,0 +1,11 @@
+import 'package:repos/src/models/deck.dart';
+
+class DeckRepo {
+  Stream<List<Deck>> getDecks() {
+    throw UnimplementedError();
+  }
+
+  Future<Deck> addNewDeck(Deck deck) {
+    throw UnimplementedError();
+  }
+}
