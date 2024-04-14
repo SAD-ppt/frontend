c4cafcc1d8500e415bc35adfc1cd7033b4f9c05f
ltp1405 <lamtungphan1405@gmail.com>
2024-04-14 22:44:44 +0700

    implement createNewNoteTemplate

--- packages/data/repos/lib/src/models/card.dart
+++ packages/data/repos/lib/src/models/card.dart
@@ -5,8 +5,6 @@ import 'package:uuid/uuid.dart';
 @immutable
 class Card extends Equatable {
   final Uuid id;
-  get getId => id;
-
   final List<(String name, String data, bool isFront)> front;
   final List<(String name, String data, bool isFront)> back;
 
--- packages/data/repos/lib/src/note_template_repo.dart
+++ packages/data/repos/lib/src/note_template_repo.dart
@@ -11,15 +11,58 @@ class NoteTemplateRepo {
   NoteTemplateRepo(
       {required this.noteTemplateApi, required this.cardTemplateApi});
 
-  /// Creates a new note template with the given [name] and [fieldNames].
-  /// The newly created note will have no associated card templates.
-  /// Returns the newly created note template.
+  void validateCardTemplates(
+    String name,
+    List<String> noteFields,
+    List<(List<String> frontFields, List<String> backFields)> cardTemplates,
+  ) {
+    var fields = Set.from(noteFields);
+    for (var i = 0; i < cardTemplates.length; i++) {
+      var (front, back) = cardTemplates[i];
+      for (var field in front) {
+        if (!fields.contains(field)) {
+          throw ArgumentError('All card templates must have the same fields');
+        }
+      }
+      for (var field in back) {
+        if (!fields.contains(field)) {
+          throw ArgumentError('All card templates must have the same fields');
+        }
+    }
+    if (name.isEmpty) {
+      throw ArgumentError('name cannot be empty');
+    } else if (cardTemplates.isEmpty) {
+      throw ArgumentError('cardTemplates cannot be empty');
+    } else if (cardTemplates.any((cardTemplate) {
+      var (front, back) = cardTemplate;
+      return front.isEmpty || back.isEmpty;
+    })) {
+      throw ArgumentError('frontFields and backFields cannot be empty');
+    }
+  }
+
   Future<void> createNewNoteTemplate(
     String name,
-    List<String> fieldNames,
+    List<String> noteFields,
+    List<(List<String> frontFields, List<String> backFields)> cardTemplates,
   ) async {
-    final (api.NoteTemplate noteTemplate, List<api.NoteTemplateField> fields) =
-        await noteTemplateApi.createNoteTemplate(name, fieldNames);
+      validateCardTemplates(name, noteFields, cardTemplates);
+      var (newNoteTemplate, fields) = await noteTemplateApi.createNoteTemplate(name, noteFields);
+      var noteFieldsIndexMap = <String, int>{};
+      for (var i = 0; i < noteFields.length; i++) {
+        noteFieldsIndexMap[noteFields[i]] = i;
+      }
+      for (var cardTemplate in cardTemplates) {
+        var (frontFields, backFields) = cardTemplate;
+        var mappedFront = frontFields.map((e) => (api.CardSide.front, noteFieldsIndexMap[e]!)).toList();
+        var mappedBack = backFields.map((e) => (api.CardSide.back, noteFieldsIndexMap[e]!)).toList();
+        var cardTemplateFields = mappedFront + mappedBack;
+        await cardTemplateApi.createNewCardTemplate(
+          newNoteTemplate.id,
+          name,
+          cardTemplateFields,
+        );
+      }
   }
 
   /// Creates a new card template with the given [name] and [front] and [back]
