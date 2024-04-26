27615d636bf314f4b9b69ba5257e80da907e0f69
zrefalogia <vuongquocphongv@gmail.com>
2024-04-26 21:33:14 +0700

    add tagsTriggered

--- packages/ui/components/lib/src/tags_list.dart
+++ packages/ui/components/lib/src/tags_list.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 class Tags extends StatelessWidget {
   final List<String> tags;
   final Function(List<String>) onTagsChanged;
-  const Tags({super.key, required this.tags, required this.onTagsChanged});
+  final VoidCallback onTagsTriggered;
+  const Tags(
+      {super.key,
+      required this.tags,
+      required this.onTagsChanged,
+      required this.onTagsTriggered});
   @override
   Widget build(BuildContext context) {
     return Card.filled(
