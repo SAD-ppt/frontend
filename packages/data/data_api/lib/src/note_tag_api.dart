d3f6315233b19229e10f1305d74261b356c5c4d8
Phan Lam Tung <lamtungpro1405@gmail.com>
2024-05-07 23:28:42 +0700

    fix naming convention

--- packages/data/data_api/lib/src/note_tag_api.dart
+++ packages/data/data_api/lib/src/note_tag_api.dart
@@ -1,11 +1,11 @@
 import 'model/note_tag.dart';
 
 abstract interface class NoteTagApi {
-  /// Returns tags associated with a card. If [cardId] is null, returns all card tags.
-  Future<List<NoteTag>> getTags(String? cardId);
+  /// Returns tags associated with a note. If [noteId] is null, returns all card tags.
+  Future<List<NoteTag>> getTags(String? noteId);
   Future<NoteTag> getTagsOfNoteByName(String noteId, String name);
   Future<NoteTag> createTag(String noteId, String name, {String? color});
   Future<NoteTag> updateTag(NoteTag tag);
   Future<void> deleteTag(String cardId, String name);
-  Future<int> getNumTagsOfCard(String cardId);
+  Future<int> getNumTagsOfNote(String noteId);
 }
