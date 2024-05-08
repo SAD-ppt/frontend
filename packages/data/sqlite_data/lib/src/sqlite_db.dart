1adb1a22d6e905605a0f739a90f425ad30a170c1
zrefalogia <vuongquocphongv@gmail.com>
2024-05-08 13:42:01 +0700

    change to sqlite_db.dart

--- packages/data/sqlite_data/lib/src/sqlite_db.dart
+++ packages/data/sqlite_data/lib/src/sqlite_db.dart
@@ -1,5 +1,4 @@
 import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqlite_data/src/card_api_handler.dart';
 import 'package:sqlite_data/src/card_template_api_handler.dart';
@@ -12,24 +11,39 @@ Future<Database> initializeDB() async {
   databaseFactory = databaseFactoryFfi;
   return openDatabase(join(await getDatabasesPath(), 'data.db'), version: 1,
       onCreate: (db, version) async {
-    // Create the tables if they don't exist
+    await db.execute(
+        'CREATE TABLE IF NOT EXISTS Deck(UniqueID TEXT PRIMARY KEY, Name TEXT, Description TEXT)');
+    await db.execute(
+        'CREATE TABLE IF NOT EXISTS NoteTemplate(UniqueID TEXT PRIMARY KEY, Name TEXT)');
+    await db.execute(
+        'CREATE TABLE IF NOT EXISTS NoteTemplateField(NoteTemplateID TEXT, OrderNumber INTEGER, Name TEXT, PRIMARY KEY(NoteTemplateID, OrderNumber), FOREIGN KEY(NoteTemplateID) REFERENCES NoteTemplate(UniqueID))');
+    await db.execute(
+        'CREATE TABLE IF NOT EXISTS Note(UniqueID TEXT PRIMARY KEY, NoteTemplateID TEXT, FOREIGN KEY(NoteTemplateID) REFERENCES NoteTemplate(UniqueID))');
+    await db.execute(
+        'CREATE TABLE IF NOT EXISTS NoteField(NoteID TEXT, OrderNumber INTEGER, RichDataText TEXT, PRIMARY KEY(NoteID, OrderNumber), FOREIGN KEY(NoteID) REFERENCES Note(UniqueID)), FOREIGN KEY(OrderNumber) REFERENCES NoteTemplateField(OrderNumber))');
+    await db.execute(
+        'CREATE TABLE IF NOT EXISTS CardTemplate(UniqueID TEXT PRIMARY KEY, NoteTemplateID TEXT, Name TEXT, FOREIGN KEY(NoteTemplateID) REFERENCES NoteTemplate(UniqueID))');
+    await db.execute(
+        'CREATE TABLE IF NOT EXISTS CardTemplateField(CardTemplateID TEXT, OrderNumber INTEGER, NoteTemplateFieldID TEXT, Side INTEGER PRIMARY KEY(CardTemplateID, OrderNumber), FOREIGN KEY(CardTemplateID) REFERENCES CardTemplate(UniqueID), FOREIGN KEY(NoteTemplateFieldID) REFERENCES NoteTemplateField(UniqueID))');
+    await db.execute(
+        'CREATE TABLE IF NOT EXISTS Card(UniqueID TEXT PRIMARY KEY, CardTemplateID TEXT, DeckID TEXT, NoteID TEXT, FOREIGN KEY(CardTemplateID) REFERENCES CardTemplate(UniqueID), FOREIGN KEY(DeckID) REFERENCES Deck(UniqueID), FOREIGN KEY(NoteID) REFERENCES Note(UniqueID))');
   });
 }
 
 class SqliteDB {
-  Future<Database>? _db;
-  CardApiHandler? cardApiHandler;
-  CardTemplateApiHandler? cardTemplateApiHandler;
-  DeckApiHandler? deckApiHandler;
-  NoteApiHandler? noteApiHandler;
-  NoteTemplateApiHandler? noteTemplateApiHandler;
+  late Database _db;
+  late CardApiHandler cardApiHandler;
+  late CardTemplateApiHandler cardTemplateApiHandler;
+  late DeckApiHandler deckApiHandler;
+  late NoteApiHandler noteApiHandler;
+  late NoteTemplateApiHandler noteTemplateApiHandler;
 
-  SqliteDB() {
-    _db = initializeDB();
-    cardApiHandler = CardApiHandler(db: _db!);
-    cardTemplateApiHandler = CardTemplateApiHandler(db: _db!);
-    deckApiHandler = DeckApiHandler(db: _db!);
-    noteApiHandler = NoteApiHandler(db: _db!);
-    noteTemplateApiHandler = NoteTemplateApiHandler(db: _db!);
+  Future<void> init() async {
+    _db = await initializeDB();
+    cardApiHandler = CardApiHandler(db: _db);
+    cardTemplateApiHandler = CardTemplateApiHandler(db: _db);
+    deckApiHandler = DeckApiHandler(db: _db);
+    noteApiHandler = NoteApiHandler(db: _db);
+    noteTemplateApiHandler = NoteTemplateApiHandler(db: _db);
   }
 }
