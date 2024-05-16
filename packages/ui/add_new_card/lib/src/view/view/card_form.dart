7161f335a4170fd918d0ea7ac320ac1e9e1537f0
zrefalogia <vuongquocphongv@gmail.com>
2024-05-16 14:21:45 +0700

    fix no available tags in add new card screen

--- packages/ui/add_new_card/lib/src/view/view/card_form.dart
+++ packages/ui/add_new_card/lib/src/view/view/card_form.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:repos/repos.dart';
 
 class CardForm extends StatelessWidget {
-
   final String deckName;
   final String noteTemplateName;
   final List<String> selectedCardTypes;
@@ -26,25 +25,26 @@ class CardForm extends StatelessWidget {
   final Function(int, String) onCardFormFieldChanged;
 
   // final VoidCallback onTagsTriggered;
-  const CardForm(
-      {super.key,
-      required this.deckName,
-      required this.onDeckChanged,
-      required this.noteTemplateName,
-      required this.onTagsChanged,
-      required this.onCardFormFieldChanged,
-      // required this.onTagsTriggered,
-      required this.tagsList,
-      required this.availableTagsList,
-      required this.selectedCardTypes,
-      required this.fieldNames,
-      required this.onNoteTemplateChanged,
-      required this.onCardTypesChanged,
-      required this.onFieldsChanged,
-      required this.availableDecks,
-      required this.availableNoteTemplates,
-      required this.availableCardTypes,
-      required this.onAddNewAvailableTag,});
+  const CardForm({
+    super.key,
+    required this.deckName,
+    required this.onDeckChanged,
+    required this.noteTemplateName,
+    required this.onTagsChanged,
+    required this.onCardFormFieldChanged,
+    // required this.onTagsTriggered,
+    required this.tagsList,
+    required this.availableTagsList,
+    required this.selectedCardTypes,
+    required this.fieldNames,
+    required this.onNoteTemplateChanged,
+    required this.onCardTypesChanged,
+    required this.onFieldsChanged,
+    required this.availableDecks,
+    required this.availableNoteTemplates,
+    required this.availableCardTypes,
+    required this.onAddNewAvailableTag,
+  });
   @override
   Widget build(BuildContext context) {
     var controller = TextEditingController();
@@ -66,9 +66,12 @@ class CardForm extends StatelessWidget {
           children: fieldNames.indexed
               .map((e) => Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [Text(e.$2), TextField(onChanged: (value) => 
-                      onCardFormFieldChanged(e.$1, value)
-                    )],
+                    children: [
+                      Text(e.$2),
+                      TextField(
+                          onChanged: (value) =>
+                              onCardFormFieldChanged(e.$1, value))
+                    ],
                   ))
               .toList(),
         ),
@@ -77,59 +80,64 @@ class CardForm extends StatelessWidget {
           children: [
             const Text('Tags',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            IconButton(onPressed: () {
-              // Show a dialog to add a new tag
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text('Add new tag'),
-                      content: newTagTextField,
-                      actions: [
-                        TextButton(
-                            onPressed: () => Navigator.of(context).pop(),
-                            child: const Text('Cancel')),
-                        TextButton(
-                            onPressed: () {
-                              if (controller.text.isNotEmpty) {
-                                onAddNewAvailableTag(controller.text);
-                              }
-                              else {
-                                // Show a snackbar to inform the user that the tag name is empty
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text('Tag name cannot be empty'),
-                                  ),
-                                );
-                              }
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text('Add')),
-                      ],
-                    );
-                  });
-            }, icon: const Icon(Icons.add)),
+            IconButton(
+                onPressed: () {
+                  // Show a dialog to add a new tag
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('Add new tag'),
+                          content: newTagTextField,
+                          actions: [
+                            TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: const Text('Cancel')),
+                            TextButton(
+                                onPressed: () {
+                                  if (controller.text.isNotEmpty) {
+                                    onAddNewAvailableTag(controller.text);
+                                  } else {
+                                    // Show a snackbar to inform the user that the tag name is empty
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content:
+                                            Text('Tag name cannot be empty'),
+                                      ),
+                                    );
+                                  }
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Add')),
+                          ],
+                        );
+                      });
+                },
+                icon: const Icon(Icons.add)),
           ],
         ),
         const SizedBox(height: 10),
-        // Tags(tags: tagsList, onTagsChanged: onTagsChanged),
-        CustomDropdown.multiSelectSearch(
-          hideSelectedFieldWhenExpanded: true,
-          hintText: 'Choose tag(s)',
-          items: availableTagsList,
-          initialItems: tagsList,
-          onListChanged: (p0) => {
-            // sort tags alphabetically
-            p0.sort(),
-            onTagsChanged(p0)
-          },
-          decoration: CustomDropdownDecoration(
-            closedBorder: Border.all(color: Colors.grey),
-            expandedBorder: Border.all(color: Colors.grey),
-            closedBorderRadius: BorderRadius.zero,
-            expandedBorderRadius: BorderRadius.zero,
-          ),
-        )
+        if (availableTagsList.isEmpty) ...[
+          const Text('No tags available'),
+        ] else ...[
+          CustomDropdown.multiSelectSearch(
+            hideSelectedFieldWhenExpanded: true,
+            hintText: 'Choose tag(s)',
+            items: availableTagsList,
+            initialItems: tagsList,
+            onListChanged: (p0) => {
+              // sort tags alphabetically
+              p0.sort(),
+              onTagsChanged(p0)
+            },
+            decoration: CustomDropdownDecoration(
+              closedBorder: Border.all(color: Colors.grey),
+              expandedBorder: Border.all(color: Colors.grey),
+              closedBorderRadius: BorderRadius.zero,
+              expandedBorderRadius: BorderRadius.zero,
+            ),
+          )
+        ],
       ],
     );
   }
